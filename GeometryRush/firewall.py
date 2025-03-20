3a6377eaeb8081031dbe4ca80b8d62b651450dc2
leresker <leresker@tuta.io>
2025-03-20 13:05:27 +0300

    firewall теперь прозрачный

--- GeometryRush/firewall.py
+++ GeometryRush/firewall.py
@@ -5,7 +5,7 @@ from entity import Entity
 class Firewall(Entity):
     def __init__(self, game):
         self.image = pygame.Surface((50, 10000))
-        self.image.fill((255, 0, 0))
+        self.image.set_alpha(0)
       
         super().__init__(self.image, pygame.math.Vector2(0, 0))
 
@@ -14,4 +14,4 @@ class Firewall(Entity):
 
     def update(self):
         self.rect = self.image.get_rect(center=self.position)
-        self.position = (self.game.position.x - self.game.screen.get_size()[0] / 2, self.game.position.y - self.image.get_size()[1]/4)
+        self.position = (self.game.position.x - self.game.screen.get_size()[0] / 2 - self.image.get_size()[0] / 2, self.game.position.y - self.image.get_size()[1]/4)
