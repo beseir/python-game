14b6048058d2c7f5118e8bc6c893ffed5399b757
leresker <leresker@tuta.io>
2025-03-24 15:39:04 +0300

    разные типы атак

--- GeometryRush/player.py
+++ GeometryRush/player.py
@@ -18,8 +18,16 @@ class Player(Entity):
         self.angle = 0
         self.rotation_speed = rotation_speed
         self.cooldown_time = 100
-        self.last_shoot_time = 0
+
+        self.attack_types = []
+        self.attack_types.append(AttackTypeBullet())
+        self.attack_types.append(AttackTypeHardBullet())
+        self.last_shoot_time = pygame.time.get_ticks()
+        self.attack_type_index = 0
         self.attack_pressed = False
+        self.attack_pressed_time = None
+        self.attack_changeing_time = None
+        self.attack_bar_background = (0, 0, 0)
      
         self.coins = 0
         self.show_health = True
@@ -67,26 +75,88 @@ class Player(Entity):
             self.rect = self.image.get_rect()
             self.rect.center = old_center
 
+    def attack_update(self):
+
+        if self.attack_pressed_time is None:
+            ratio = float(pygame.time.get_ticks() - self.last_shoot_time) / float(self.cooldown_time)
+
+            self.set_bar(float(min(ratio, 1.0)), self.attack_bar_background, (255, 255, 255))
+            if ratio > 1.0:
+                self.attack_bar_background = (255, 255, 255)
+        else:
+            if (pygame.time.get_ticks() - self.attack_changeing_time > self.attack_types[self.attack_type_index].time_to_load * 1000) and len(self.attack_types) > self.attack_type_index + 1:
+                self.attack_changeing_time += self.attack_types[self.attack_type_index].time_to_load * 1000
+                self.attack_bar_background = self.attack_types[self.attack_type_index].color
+                self.attack_type_index += 1
+            
+            ratio = float(pygame.time.get_ticks() - self.attack_changeing_time) / float(self.attack_types[self.attack_type_index].time_to_load * 1000)
+            self.set_bar(min(ratio, 1.0), self.attack_bar_background, self.attack_types[self.attack_type_index].color)
+
+        
+        if not self.input_controller.attack and self.attack_pressed and pygame.time.get_ticks() - self.last_shoot_time >= self.cooldown_time:
+            # кнопка атаки отпустилась
+            self.attack_pressed_time = None
+            self.attack_changeing_time = None
+
+            self.last_shoot_time = pygame.time.get_ticks()
+            self.attack_types[self.attack_type_index].attack(self, self.game)
+            self.attack_type_index = 0
+
+            
+
+        if self.input_controller.attack and not self.attack_pressed:
+            # кнопка атаки нажалась
+            self.attack_pressed_time = pygame.time.get_ticks()
+            self.attack_changeing_time = pygame.time.get_ticks()
+        
+        self.attack_pressed = self.input_controller.attack
+            
 
     def update(self):
 
         self.update_rotation(self.input_controller.view_direction)
         self.update_position(self.input_controller.movement_direction)
+        self.attack_update()
 
-        self.set_bar(min(float(pygame.time.get_ticks() - self.last_shoot_time) / float(self.cooldown_time), 1.0), (255, 255, 255), (0, 0, 0))
-        
-        if not self.input_controller.attack and self.attack_pressed and pygame.time.get_ticks() - self.last_shoot_time >= self.cooldown_time:
-            self.last_shoot_time = pygame.time.get_ticks()
-            power = min(self.coins, 10)
-            self.coins -= power
-            power = max(1, power)
-            self.game.add_bullet(Bullet(self.position, self.input_controller.view_direction, power, self))
-        
-        self.attack_pressed = self.input_controller.attack
-            
-        
         if (self.health <= 0):
             self.drop_coins(self.coins)
             self.kill()
 
         self.rect.center = self.position
+
+
+class AttackType:
+    def __init__(self):
+        self.time_to_load = 0.0
+        self.color = (0, 0, 0)
+    
+    def attack(self, entity, game, ):
+        pass
+
+class AttackTypeBullet:
+    def __init__(self):
+        self.time_to_load = 1.0
+        self.color = (0, 0, 0)
+
+    def attack(self, entity, game):
+        if hasattr(entity, "coins"):
+            power = min(entity.coins, 5)
+            entity.coins -= power
+            power = max(1, power)
+        else:
+            power = 1
+        game.add_bullet(Bullet(entity.position, entity.input_controller.view_direction, power, entity))
+
+class AttackTypeHardBullet:
+    def __init__(self):
+        self.time_to_load = 2.0
+        self.color = (200, 0, 0)
+
+    def attack(self, entity, game):
+        if hasattr(entity, "coins"):
+            power = min(entity.coins, 20)
+            entity.coins -= power
+            power = max(5, power)
+        else:
+            power = 5
+        game.add_bullet(Bullet(entity.position, entity.input_controller.view_direction, power, entity))
